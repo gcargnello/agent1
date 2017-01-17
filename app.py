64d114d0bb1ef2aef85ff98d757ce86d3040b6a5
Giuliano Cargnello <giuliano.cargnello@gmail.com>
2017-01-17 22:09:52 +0100

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import json
 import os
 import requests
+from  routines import getWitz
 
 from flask import Flask
 from flask import request
@@ -61,48 +62,6 @@ def makeWebhookResult(req):
         "source": "Agent1"
     }
 
-# legge una  barzelletta chcuck
-def getWitz(g):
-    # PROCEDURA MAIN recupera un witz
-    if g == 'any':
-        url = 'http://api.icndb.com/jokes/random'
-    else:
-        url = 'http://api.icndb.com/jokes/random?limitTo=[nerdy]'
-#        url = 'http://api.icndb.com/jokes/random/?limitTo[' + g +']'
-        print(url)
-
-    r = requests.get(url)
-
-    if r.status_code != 200:
-        pass
-        witz = 'Non ne ho una da raccontare...'
-    else:
-        full_json = r.text
-        full = json.loads(full_json)
-        witz = (full['value']['joke'])
-
-    # togli i cartteri escape
-    witz = html_decode(witz)
-    print ('W:', witz)
-
-    return witz
-
-# escape decode
-def html_decode(s):
-    """
-    Returns the ASCII decoded version of the given HTML string. This does
-    NOT remove normal HTML tags like <p>.
-    """
-    htmlCodes = (
-            ("'", '&#39;'),
-            ('"', '&quot;'),
-            ('>', '&gt;'),
-            ('<', '&lt;'),
-            ('&', '&amp;')
-        )
-    for code in htmlCodes:
-        s = s.replace(code[1], code[0])
-    return s
 
 # Statement standard Flask per avviamento in localhost
 if __name__ == '__main__':
---
+++ routines.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+# legge una  barzelletta chcuck
+def getWitz(g):
+    # PROCEDURA MAIN recupera un witz
+    if g == 'any':
+        url = 'http://api.icndb.com/jokes/random'
+    else:
+ #       url = 'http://api.icndb.com/jokes/random?limitTo=[nerdy]'
+        url = 'http://api.icndb.com/jokes/random?limitTo=[' + g +']'
+        print(url)
+
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        pass
+        witz = 'Non ne ho una da raccontare...'
+    else:
+        full_json = r.text
+        full = json.loads(full_json)
+        witz = (full['value']['joke'])
+
+    # togli i cartteri escape
+    witz = html_decode(witz)
+    print ('W:', witz)
+
+    return witz
+
+# escape decode
+def html_decode(s):
+    """
+    Returns the ASCII decoded version of the given HTML string. This does
+    NOT remove normal HTML tags like <p>.
+    """
+    htmlCodes = (
+            ("'", '&#39;'),
+            ('"', '&quot;'),
+            ('>', '&gt;'),
+            ('<', '&lt;'),
+            ('&', '&amp;')
+        )
+    for code in htmlCodes:
+        s = s.replace(code[1], code[0])
+    return s
